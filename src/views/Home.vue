c53929e53c590562484fc1d0fc528bf593d6fa56
Joel Lithgow <joelnotlithgow@gmail.com>
2021-03-04 00:36:38 -0700

    Changed thing?

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,86 @@
 <template>
-  <div class="home">
-    <ul class="hex-grid__list">
-      <li v-for="(tile, index) in tiles" :key="index" class="hex-grid__item">
-        <div class="hex-grid__content">
-          {{ index }}
+  <div class="home hex-grid" id="hex-grid">
+    <section class="hex-grid__list">
+      <div v-for="(col, x) in tileArr" :key="x" class="hex-grid__item">
+        <div v-for="(row, y) in col" :key="x-y" class="hex-grid__item">
+          <div @click="tileClick(x, y)" 
+               class="hex-grid__content"
+               :class="[ tileArr[x][y].type ]">
+          </div>
         </div>
-      </li>
-    </ul>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
+function getRandom(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+class Zone {
+  constructor(base) {
+    this.base = base;
+    this.x = base.x;
+    this.y = base.y;
+    this.group = base.group;
+    this.tiles = [];
+  }
+
+  addAll() {
+   alert("whoops"); 
+  }
+}
+let zone = new Zone({x:1,y:3,type:2,group:2});
+console.log(zone);
+
+class Tile {
+  constructor(x, y, type, group) {
+    this.x = x;
+    this.y = y;
+    this.group = group;
+    if (!isNaN(type)) {
+      switch (Number(type)) {
+        case 0:
+          this.type = "water";
+          break;
+        case 1:
+          this.type = "grass";
+          break;
+        case 2:
+          this.type = "tree";
+          break;
+      }
+    } else {
+      this.type = type;
+    }
+  }
+}
 
 export default {
   name: 'Home',
   data() {
     return {
-      tiles: [1, 2, 3, 4, 5, 6, 7 , 8 , 9 ,10 , 11, 12, 13, 14, 15]
+      selected: Object,
+      colAmount: 4,
+      rowAmount: 10,
+      tileArr: []
     }
   },
+  methods: {
+    tileClick(col, row) {
+      alert("test!"+col+row);
+    }
+  },
+  created() {
+    for (let x=0; x<this.colAmount; x++) {
+      this.tileArr.push([]);
+      for (let y=0; y<this.rowAmount; y++) {
+        this.tileArr[x].push(new Tile(x, y, "1", getRandom(6)));
+      }
+    }
+  }
 }
 </script>
 
@@ -31,6 +91,14 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.grass {
+  background-color: green;
+}
+
+.empty {
+  background-color: white;
 }
 
 $block: '.hex-grid';
@@ -53,46 +121,44 @@ $block: '.hex-grid';
 }
 
 #{$block} {
-  display: flex;
-  justify-content: center;
 
-  &__list {
-    --amount: 5;
-    position: relative;
-    padding: 0;
-    margin: 0;
-    list-style-type: none;
-    display: grid;
-    grid-template-columns: repeat(var(--amount), 1fr 2fr) 1fr;
-    grid-gap: 2.5rem 5rem;
-  }
+    &__list {
+      --amount: 5;
+      position: relative;
+      padding: 0;
+      margin: 0;
+      list-style-type: none;
+      display: grid;
+      grid-template-columns: repeat(var(--amount), 1fr 2fr) 1fr;
+      grid-gap: 2.5rem 5rem;
+    }
 
-  &__item {
-    position: relative;
-    grid-column: 1 / span 3;
-    grid-row: calc(var(--counter) + var(--counter)) / span 2;
-    filter: drop-shadow(0 0 10px rgba(#444, .08));
-    height: 0;
-    padding-bottom: 90%;
-  }
+    &__item {
+      position: relative;
+      grid-column: 1 / span 3;
+      grid-row: calc(var(--counter) + var(--counter)) / span 2;
+      filter: drop-shadow(0 0 10px rgba(#444, .08));
+      height: 0;
+      padding-bottom: 90%;
+    }
 
-  &__content {
-    position: absolute;
-    height: 100%;
-    width: 100%;
-    font-size: 1.125rem;
-    color: #111111;
-    background-color: white;
-    clip-path: polygon(75% 0, 100% 50%, 75% 100%, 25% 100%, 0 50%, 25% 0);
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 2rem 25%;
-    text-decoration: none;
-    text-align: center;
-    transition: transform .24s ease-out;
-  }
+    &__content {
+      position: absolute;
+      height: 100%;
+      width: 100%;
+      font-size: 1.125rem;
+      color: #111111;
+      clip-path: polygon(75% 0, 100% 50%, 75% 100%, 25% 100%, 0 50%, 25% 0);
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      padding: 0;
+      margin: 0;
+      text-decoration: none;
+      text-align: center;
+      transition: transform .24s ease-out;
+    }
 }
 
 @media screen and (min-width: 1440px) {
@@ -124,13 +190,13 @@ $block: '.hex-grid';
 @media screen and (min-width: 840px) and (max-width: 1119px) {
 	#{$block} {
 		&__list {
-      --amount: 3;
-			--counter: 1;
+      --amount: 4;
+			--counter: 4;
 			grid-gap: 1.5rem 3rem;
 		}
 
 		&__item {
-			@include grid-item(3);
+			@include grid-item(4);
 		}
 	}
 }
@@ -152,13 +218,15 @@ $block: '.hex-grid';
 @media screen and (max-width: 479px) {
 	#{$block} {
 		&__list {
-      --amount: 5;
-			grid-gap: 1.5rem 3rem;
+      --amount: 4;
+			--counter: 1;
+			grid-gap: 0;
 		}
 
 		&__item {
-			@include grid-item(5);
+			@include grid-item(4);
 		}
 	}
 }
+
 </style>
